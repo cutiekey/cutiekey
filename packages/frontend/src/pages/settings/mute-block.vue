dd22d7b6830403858ced87d2f871bff6bd3801e0
Amelia Yukii <admin@transfem.org>
2023-12-27 10:16:22 +0100

    chore: update hard word mute icon

--- packages/frontend/src/pages/settings/mute-block.vue
+++ packages/frontend/src/pages/settings/mute-block.vue
@@ -13,7 +13,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	</MkFolder>
 
 	<MkFolder>
-		<template #icon><i class="ti ti-message-off"></i></template>
+		<template #icon><i class="ph-x-square ph-bold ph-lg"></i></template>
 		<template #label>{{ i18n.ts.hardWordMute }}</template>
 
 		<XWordMute :muted="$i!.hardMutedWords" @save="saveHardMutedWords"/>
