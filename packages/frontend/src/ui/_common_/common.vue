2e0557c90c6826a50b9f788a8d818d4476f4f6b9
ShittyKopper <shittykopper@w.on-t.work>
2024-02-02 14:56:58 +0300

    upd: lazy load SkOneko on demand

--- packages/frontend/src/ui/_common_/common.vue
+++ packages/frontend/src/ui/_common_/common.vue
@@ -60,7 +60,8 @@ import { useStream } from '@/stream.js';
 import { i18n } from '@/i18n.js';
 import { defaultStore } from '@/store.js';
 import { globalEvents } from '@/events.js';
-import SkOneko from '@/components/SkOneko.vue';
+
+const SkOneko = defineAsyncComponent(() => import('@/components/SkOneko.vue'));
 
 const XStreamIndicator = defineAsyncComponent(() => import('./stream-indicator.vue'));
 const XUpload = defineAsyncComponent(() => import('./upload.vue'));
