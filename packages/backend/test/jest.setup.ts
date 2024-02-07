20fe4a80dc3f5cca358f583246db03e1be4f334a
Luna Nova <her@mint.lgbt>
2024-02-07 07:56:46 -0500

    chore: nuke everything

--- packages/backend/test/jest.setup.ts
+++
@@ -1,8 +0,0 @@
-import { initTestDb, sendEnvResetRequest } from './utils.js';
-
-beforeAll(async () => {
-	await Promise.all([
-		initTestDb(false),
-		sendEnvResetRequest(),
-	]);
-});
--- packages/frontend/src/scripts/media-has-audio.ts
+++
@@ -1,9 +0,0 @@
-export default async function hasAudio(media: HTMLMediaElement) {
-	const cloned = media.cloneNode() as HTMLMediaElement;
-	cloned.muted = (cloned as typeof cloned & Partial<HTMLVideoElement>).playsInline = true;
-	cloned.play();
-	await new Promise((resolve) => cloned.addEventListener('playing', resolve));
-	const result = !!(cloned as any).audioTracks?.length || (cloned as any).mozHasAudio || !!(cloned as any).webkitAudioDecodedByteCount;
-	cloned.remove();
-	return result;
-}
