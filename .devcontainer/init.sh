5f94a003eef6a30db48add2445bc29b61b36e393
Luna Nova <her@mint.lgbt>
2024-02-28 14:07:57 -0500

    feat: update Dev Container files

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -6,6 +6,6 @@ sudo chown -R node /workspace
 git submodule update --init
 pnpm config set store-dir /home/node/.local/share/pnpm/store
 pnpm install --frozen-lockfile
-cp .devcontainer/devcontainer.yml .config/default.yml
-pnpm build
-pnpm migrate
+cp ./.devcontainer/devcontainer.yml ./.config/default.yml
+pnpm run build
+pnpm run migrate
